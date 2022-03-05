640b05a1648cd5f22d39067128df1c4ffbf166a7
Maxpayne <ferhatadibelli9@gmail.com>
2022-03-05 21:16:10 +0300

    starting work

---
+++ first-node.js
@@ -0,0 +1,29 @@
+let x = "ferhat";
+let age = 25;
+
+function myFun() {
+  return x + age;
+}
+
+console.log(myFun());
+
+let person = {
+  name: "ferhat",
+  age: 27,
+  greeting: () => {
+    return person.name;
+  },
+};
+
+console.log(person.greeting());
+
+
+
+
+
+
+
+
+
+
+
